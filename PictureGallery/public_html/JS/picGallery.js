515b0e10b571416d79c6942fba7b9f3b28dc9591
Olin <Olin@192.168.1.13>
2014-12-30 10:55:36 -0900

    Added Bootstrap functionality to improve responsive design

--- PictureGallery/public_html/JS/picGallery.js
+++ PictureGallery/public_html/JS/picGallery.js
@@ -2,16 +2,18 @@
 /* I have had no experience with JSON or JQuery so I went to YouTube and the internet to 
  * find info. Although it is not working, I feel it is close.  
  */
-    
-    $.getJSON( "picGallery.json", function( data ) {
-var items = [];
-$.each( data, function( key, val ) {
-items.push( "<a href='" + key + "'>");
-});
-$( "<ul/>", {
-"class": "pictures",
-html: items.join( "" )
-}).appendTo( "body" );
+$(document).ready(function () {
+
+    $.getJSON("picGallery.json", function (data) {
+        var items = [];
+        $.each(data, function (key, val) {
+            items.push("<a href='" + key + "'>");
+        });
+        $("<ul/>", {
+            "class": "pictures",
+            html: items.join("")
+        }).appendTo("body");
+    });
 });
 
 
